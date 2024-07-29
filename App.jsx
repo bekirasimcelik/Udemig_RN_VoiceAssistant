baeb9ea42afa20ce3e395ebb17f24a33f7af87cc
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-07-29 23:50:17 +0300

    packages uploaded and nativeWind integrated

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text className="bg-red-500">App</Text>
+    </SafeAreaView>
+  );
+}
