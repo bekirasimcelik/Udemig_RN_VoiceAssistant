ac0e2547e86c863a8eb45af2622b0996ae331aa7
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-07-31 12:19:46 +0300

    responsive lib added

--- src/screens/WelcomeScreen.jsx
+++ src/screens/WelcomeScreen.jsx
@@ -1,15 +1,42 @@
-import {View, Text, SafeAreaView} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
+import {View, Text, SafeAreaView, Image, TouchableOpacity} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 export default function WelcomeScreen() {
+  const navigation = useNavigation();
   return (
     <SafeAreaView className="flex-1 flex justify-around bg-white">
       <View className="space-y-2">
-        <Text className="text-center text-4xl font-bold text-gray-700">
+        <Text
+          style={{fontSize: wp(10)}}
+          className="text-center font-bold text-gray-700">
           Jarvis
         </Text>
-        <Text>The Future is here, powered by AI.</Text>
+        <Text
+          style={{fontSize: wp(4)}}
+          className="text-center tracking-wider text-gray-600 font-semibold">
+          The Future is here, powered by AI.
+        </Text>
       </View>
+      <View className="flex-row justify-center">
+        <Image
+          source={require('../assets/images/welcome.png')}
+          style={{width: wp(75), height: wp(75)}}
+        />
+      </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Home')}
+        className="bg-emerald-600 mx-5 p-4 rounded-2xl">
+        <Text
+          style={{fontSize: wp(6)}}
+          className="text-center font-bold text-white">
+          Get Started
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
