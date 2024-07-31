3a9d9de89e56f37e932b78a949781fb87595932c
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-07-31 17:14:29 +0300

    homeScreen updated

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,10 +1,21 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {View, Text, SafeAreaView, Image} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1 text-white">
+      <SafeAreaView className="flex-1 flex mx-5">
+        <View className="flex-row justify-center">
+          <Image
+            source={require('../assets/images/JarvisBot.gif')}
+            style={{height: hp(15), width: hp(15)}}
+          />
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
