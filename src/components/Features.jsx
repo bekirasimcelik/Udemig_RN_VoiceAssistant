56388dc8f844b32366a6567b846ea58e58842879
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-08-01 10:52:17 +0300

    homeScreen updated

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -21,9 +21,49 @@ export default function Features() {
           <Text
             style={{fontSize: wp(4.8)}}
             className="font-semibold text-gray-700">
-            Chat GPT
+            ChatGPT
           </Text>
         </View>
+        <Text style={{fontSize: wp(3.8)}} className="text-gray-700 font-medium">
+          ChatGPT can provide you with instant and knowledge responses, assist
+          you with creative idead on a wide range of topics.
+        </Text>
+      </View>
+      <View className="bg-purple-200 p-4 rounded-xl space-y-2">
+        <View className="flex-row items-center space-x-1">
+          <Image
+            source={require('../assets/images/dall-e.png')}
+            style={{width: hp(4), height: hp(4)}}
+            className="rounded-md"
+          />
+          <Text
+            style={{fontSize: wp(4.8)}}
+            className="font-semibold text-gray-700">
+            Dall-E
+          </Text>
+        </View>
+        <Text style={{fontSize: wp(3.8)}} className="text-gray-700 font-medium">
+          Dell-E can provide you with instant and knowledge responses, assist
+          you with creative idead on a wide range of topics.
+        </Text>
+      </View>
+      <View className="bg-cyan-200 p-4 rounded-xl space-y-2">
+        <View className="flex-row items-center space-x-1">
+          <Image
+            source={require('../assets/images/smartAI.png')}
+            style={{width: hp(4), height: hp(4)}}
+            className="rounded-md"
+          />
+          <Text
+            style={{fontSize: wp(4.8)}}
+            className="font-semibold text-gray-700">
+            Smart AI
+          </Text>
+        </View>
+        <Text style={{fontSize: wp(3.8)}} className="text-gray-700 font-medium">
+          Smart AI can provide you with instant and knowledge responses, assist
+          you with creative idead on a wide range of topics.
+        </Text>
       </View>
     </View>
   );
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
-import {View, Text, SafeAreaView, Image} from 'react-native';
+import {View, Text, SafeAreaView, Image, ScrollView} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import Features from '../components/Features';
+import {dummyMessages} from '../constants';
 
 export default function HomeScreen() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState(dummyMessages);
   return (
     <View className="flex-1 text-white">
       <SafeAreaView className="flex-1 flex mx-5">
@@ -17,7 +18,63 @@ export default function HomeScreen() {
             style={{height: hp(15), width: hp(15)}}
           />
         </View>
-        {messages.length > 0 ? <View></View> : <Features />}
+        {messages.length > 0 ? (
+          <View className="space-y-2 flex-1">
+            <Text
+              style={{fontSize: wp(5)}}
+              className="text-gray-700 font-semibold ml-1">
+              Assistant
+            </Text>
+            <View
+              style={{height: hp(58)}}
+              className="bg-neutral-200 rounded-3xl p-4">
+              <ScrollView
+                bounces={false}
+                className="space-y-4"
+                showsVerticalScrollIndicator={false}>
+                {messages.map((message, index) => {
+                  if (message.role === 'assistant') {
+                    if (message.content.includes('https')) {
+                      return (
+                        <View key={index} className="flex-row justify-start">
+                          <View className="p-2 flex rounded-2xl bg-emerald-100 rounded-tl-none">
+                            <Image
+                              source={{uri: message.content}}
+                              className="rounded-2xl"
+                              resizeMode="contain"
+                              style={{height: wp(60), width: wp(60)}}
+                            />
+                          </View>
+                        </View>
+                      );
+                    } else {
+                      return (
+                        <View
+                          key={index}
+                          style={{width: wp(70)}}
+                          className="bg-emerald-100 rounded-xl p-2 rounded-tl-none">
+                          <Text>{message.content}</Text>
+                        </View>
+                      );
+                    }
+                  } else {
+                    return (
+                      <View key={index} className="flex-row justify-end">
+                        <View
+                          style={{width: wp(70)}}
+                          className="bg-white rounded-xl p-2 rounded-tr-none">
+                          <Text>{message.content}</Text>
+                        </View>
+                      </View>
+                    );
+                  }
+                })}
+              </ScrollView>
+            </View>
+          </View>
+        ) : (
+          <Features />
+        )}
       </SafeAreaView>
     </View>
   );
