554a444cfb79cec9b3ed7132a18b915f79406806
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-08-02 21:37:49 +0300

    voice implemented

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,14 +1,76 @@
-import React, {useState} from 'react';
-import {View, Text, SafeAreaView, Image, ScrollView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import Features from '../components/Features';
 import {dummyMessages} from '../constants';
+import Voice from '@react-native-community/voice';
 
 export default function HomeScreen() {
   const [messages, setMessages] = useState(dummyMessages);
+  const [recording, setRecording] = useState(false);
+  const [speaking, setSpeaking] = useState(true);
+
+  const speechStartHandler = e => {
+    console.log('speech start handler');
+  };
+  const speechEndHandler = e => {
+    setRecording(false);
+    console.log('speech end handler');
+  };
+  const speechResultHandler = e => {
+    console.log('voice event: ', e);
+  };
+  const speechErrorHandler = e => {
+    console.log('speech error handler: ', e);
+  };
+  const startRecording = async () => {
+    setRecording(true);
+    try {
+      await Voice.start('en-GB'); // en-US
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  };
+  const stopRecording = async () => {
+    try {
+      await Voice.stop();
+      setRecording(false);
+      // fecth response
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  };
+
+  const clear = () => {
+    setMessages([]);
+  };
+
+  const stopSpeaking = () => {
+    setSpeaking(false);
+  };
+
+  useEffect(() => {
+    // Voice handler events
+    Voice.onSpeechStart = speechStartHandler;
+    Voice.onSpeechEnd = speechEndHandler;
+    Voice.onSpeechResults = speechResultHandler;
+    Voice.onSpeechError = speechErrorHandler;
+
+    return () => {
+      // destroy the voice instance
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
   return (
     <View className="flex-1 text-white">
       <SafeAreaView className="flex-1 flex mx-5">
@@ -75,6 +137,43 @@ export default function HomeScreen() {
         ) : (
           <Features />
         )}
+        {/* recording, clear and stop buttons */}
+        <View className="flex justify-center items-center">
+          {recording ? (
+            <TouchableOpacity>
+              {/* recording stop button */}
+              onPress={stopRecording}
+              <Image
+                className="rounded-full"
+                source={require('../assets/images/voiceRecord.png')}
+                style={{width: hp(10), height: hp(10)}}
+              />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity onPress={startRecording}>
+              {/* recording start button */}
+              <Image
+                className="rounded-full"
+                source={require('../assets/images/mic.png')}
+                style={{width: hp(10), height: hp(10)}}
+              />
+            </TouchableOpacity>
+          )}
+          {messages.length > 0 && (
+            <TouchableOpacity
+              onPress={clear}
+              className="bg-neutral-400 rounded-3xl p-2 absolute right-10">
+              <Text className="text-white font-semibold">Clear</Text>
+            </TouchableOpacity>
+          )}
+          {speaking && (
+            <TouchableOpacity
+              onPress={stopSpeaking}
+              className="bg-red-400 rounded-3xl p-2 absolute left-10">
+              <Text className="text-white font-semibold">Stop</Text>
+            </TouchableOpacity>
+          )}
+        </View>
       </SafeAreaView>
     </View>
   );
