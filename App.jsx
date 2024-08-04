658b060def18055abc0bc0414714a07b0c2737b4
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-08-04 18:29:55 +0300

    openAI.js updated

--- App.jsx
+++ App.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AppNavigation from './src/navigation';
+import {apiCall} from './src/api/openAI';
 
 export default function App() {
+  useEffect(() => {
+    // apiCall('what is quantum computing');
+  }, []);
   return <AppNavigation />;
 }
--- src/api/openAI.js
+++ src/api/openAI.js
@@ -24,7 +24,48 @@ export const apiCall = async (prompt, messages) => {
       ],
     });
 
-    console.log('data :, res.data');
+    console.log('data: ', res.data.choices[0].message);
+    let isArt = res.data?.choices[0]?.message?.content;
+    if (isArt.toLowerCase().includes('yes')) {
+      console.log('dalle api call');
+      return dalleApiCall(prompt, messages || []);
+    } else {
+      console.log('chat gpt api call');
+      return chatgptApiCall(prompt, messages || []);
+    }
+  } catch (err) {
+    console.log('error: ', err);
+    return Promise.resolve({success: false, msg: err.message});
+  }
+};
+
+const chatgptApiCall = async (prompt, messages) => {
+  try {
+    const res = await client.post(chatGPTEndpoint, {
+      model: 'gpt-3.5-turbo',
+      messages,
+    });
+    let answer = res.data?.choices[0]?.message?.content;
+    messages.push({role: 'assistant', content: answer.trim()});
+  } catch (err) {
+    console.log('error: ', err);
+    return Promise.resolve({success: false, msg: err.message});
+  }
+};
+
+const dalleApiCall = async (prompt, messages) => {
+  try {
+    const res = await client.post(dalleEndpoint, {
+      model: 'dall-e-3',
+      prompt,
+      n: 1,
+      size: '512x512',
+    });
+
+    let url = res?.data?.data[0]?.url;
+    console.log('got url of the image: ', url);
+    messages.push({role: 'assistant', content: url});
+    return Promise.resolve({success: true, data: messages});
   } catch (err) {
     console.log('error: ', err);
     return Promise.resolve({success: false, msg: err.message});
